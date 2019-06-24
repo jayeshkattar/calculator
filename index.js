4162ea28158f22d89fb7a9f91ed44c66f1b56868
Jayesh KR <kr.jayesh94@gmail.com>
2019-06-24 22:19:12 +0530

    template

---
+++ index.js
@@ -0,0 +1,7 @@
+// Swetha = ADD
+// Amit = Substract
+// sreecharan = multiply
+// charan kuna = divide
+// sasi = modulus
+
+// write five different functions - 2 params - comput code - return the computed value
